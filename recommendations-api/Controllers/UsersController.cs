cea2828c9f1872516eb67cc7678ea2d881ded409
Ronald Ding <roding@microsoft.com>
2018-08-28 10:40:52 -0500

    Added controller

--- recommendations-api/Controllers/UsersController.cs
+++ recommendations-api/Controllers/UsersController.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Cryptography.Algorithms;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace recommendations_api.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController : Controller
     {
-        
+        [Route("api/createuser")]
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] dynamic value)
+        {
+            string firstName = (string) value.firstName;
+            string lastName = (string) value.lastName;
+            string username = (string) value.username;
+            string password = (string) value.password;
 
+            return Json(new { name = "Ronald Ronald Ronald" });
+        }
+
+        [Route("api/signin")]
+        public IActionResult SignIn([FromBody] dynamic value)
+        {
+            return Json(new { });
+        }
+        [Route("api/signout")]
+        public IActionResult SignOut([FromBody] dynamic value)
+        {
+            return Json(new { });
+        }
     }
 }
